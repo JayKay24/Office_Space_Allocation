67c248c03ad982224268fb5b5684b5e33180360d
James Kinyua <jameskinyua590@gmail.com>
2017-04-10 21:52:23 +0300

    Create properties offices and living_spaces in class Dojo

--- dojo.py
+++ dojo.py
@@ -1,6 +1,18 @@
-class Dojo():
+import 
+class Dojo:
     """
-    This class initializes Dojo objects.
+    This class accepts a list of offices and living spaces objects and
+    initializes Dojo objects.
     """
-    def __init__(self):
-        pass
+    def __init__(self, offices, living_spaces):
+        self.__offices = offices
+        self.__living_spaces = living_spaces
+
+    @property
+    def offices(self):
+        return len(self.__offices)
+
+    @property
+    def living_spaces(self):
+        return len(self.__living_spaces)
+    
