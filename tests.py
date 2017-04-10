0eb7b52b9ac0c8df2858ef8a967c48fb57cb789b
James Kinyua <jameskinyua590@gmail.com>
2017-04-10 22:05:17 +0300

    Add test case testLivingSpaceHasAttribute in tests.py

--- tests.py
+++ tests.py
@@ -35,10 +35,13 @@ class RoomTest(unittest.TestCase):
 
 class LivingSpaceTest(unittest.TestCase):
     def setUp(self):
-        self.living_space = living_space.LivingSpace()
+        self.living_space = living_space.LivingSpace("Black")
 
     def testLivingSpaceHasMaxNum4(self):
         self.assertEqual(self.living_space.max_num, 4)
+
+    def testLivingSpaceHasNameAttribute(self):
+        self.assertEqual(self.living_space.name, "Black")
 
 class OfficeTest(unittest.TestCase):
     def setUp(self):
