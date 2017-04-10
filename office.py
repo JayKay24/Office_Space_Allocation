f030d04bd352f7387c432f1d4c8155885bb0627a
James Kinyua <jameskinyua590@gmail.com>
2017-04-10 16:21:09 +0300

    Create class OfficeTest in tests.py

--- office.py
+++ office.py
@@ -4,5 +4,5 @@ class Office(room.Room):
     """
     This class is used to initialize Office objects.
     """
-    def __init__(self, max_num=4):
+    def __init__(self, max_num=6):
         self.max_num = max_num
