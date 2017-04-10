b2e2f4dcaab6944cccbf4209a366f9bfb451f43d
James Kinyua <jameskinyua590@gmail.com>
2017-04-10 17:16:56 +0300

    Add attribute name in class Room

---
+++ controller.py
@@ -0,0 +1,8 @@
+from dojo import Dojo
+from fellow import Fellow
+from living_space import LivingSpace
+from office import Office
+from person import Person
+from room import Room
+from staff import Staff
+
