03d68b31b46d08479456b0b4c068828abc2b06eb
James Kinyua <jameskinyua590@gmail.com>
2017-04-10 17:47:56 +0300

    Add a docstring to each function

--- controller.py
+++ controller.py
@@ -9,16 +9,29 @@ from staff import Staff
 # Init.
 people = []
 def create_office(name):
+    """
+    Accepts argument name to create an Office object.
+    """
     office = Office(name)
     return office
+
 def create_living_space(name):
+    """
+    Accepts argument name to create a LivingSpace object.
+    """
     living_space = LivingSpace(name)
     return living_space
 
 def create_person(name, status):
+    """
+    Accepts arguments name, status to create a Person object.
+    """
     person = Person(name, status)
     return person
 
 def add_person(person):
+    """
+    Accepts an object to append to the people list.
+    """
     people.append(person)
     return people
