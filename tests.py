50a35ff10bde1d3b4b36554b2dfa91638498d69e
James Kinyua <jameskinyua590@gmail.com>
2017-04-10 15:27:51 +0300

    Create file tests.py and create class PersonTest

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+import person
+import fellow
+import living_space
+import staff
+import dojo
+import room
+import office
+
+class PersonTest(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
