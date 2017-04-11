9add7449c42abc41d2ee6e4de35b6f372083959d
James Kinyua <jameskinyua590@gmail.com>
2017-04-11 21:24:27 +0300

    Define a property spaces_left

--- office.py
+++ office.py
@@ -7,3 +7,8 @@ class Office(room.Room):
     def __init__(self, name, max_num=6):
         self.name = name
         self.max_num = max_num
+        self.__spaces_left = 6
+
+    @property
+    def spaces_left(self):
+    	return __spaces_left
