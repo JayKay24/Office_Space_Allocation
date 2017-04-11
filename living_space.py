d58fb9182031448a6a6b4e2e511c4cac4449f53e
James Kinyua <jameskinyua590@gmail.com>
2017-04-11 21:20:42 +0300

    Define a property spaces_left in class LivingSpace

--- living_space.py
+++ living_space.py
@@ -7,4 +7,11 @@ class LivingSpace(room.Room):
     def __init__(self, name, max_num=4):
         self.name = name
         self.max_num = max_num
+        self.__spaces_left = 4
+
+    @property
+    def spaces_left(self):
+    	return __spaces_left
+
+    
         
