8499df3fcbc760a20d4c588ff27fe42cc77bb9d7
James Kinyua <jameskinyua590@gmail.com>
2017-04-12 21:53:43 +0300

    Delete file test_create_office.py

--- tests/test_office.py
+++ tests/test_office.py
@@ -10,5 +10,14 @@ class OfficeTest(unittest.TestCase):
     def testOfficeHasAttributeName(self):
         self.assertEqual(self.office.name, "Blue")
 
+class CreateOfficeTests(unittest.TestCase):
+    def setUp(self):
+        self.name = "Blue"
+
+    def testCreateOffice(self):
+        self.office = create_office(self.name)
+        self.assertEqual(self.office.name, "Blue")
+
+
 if __name__ == "__main__":
 	unittest.main()
